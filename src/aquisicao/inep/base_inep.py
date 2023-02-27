11f5fdb88d012815f1f38f4058d7b6eb67186107
Caio Raja <caioraja@outlook.com>
2023-02-27 15:45:34 -0300

    Remove métodos redefinidos

--- src/aquisicao/inep/base_inep.py
+++ src/aquisicao/inep/base_inep.py
@@ -80,17 +80,3 @@ class BaseINEPETL(abc.ABC):
         """
         pass
 
-    def load(self) -> None:
-        """
-        Exporta os dados transformados
-        """
-        for arq, df in self.dados_saida.items():
-            df.to_parquet(self.caminho_saida / arq, index=False)
-
-    def pipeline(self) -> None:
-        """
-        Executa o pipeline completo de tratamento de dados
-        """
-        self.extract()
-        self.transform()
-        self.load()
