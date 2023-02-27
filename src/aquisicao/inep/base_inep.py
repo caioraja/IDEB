065211b1d09054ebebd8baa59d91b8a9da9e42a9
Caio Raja <caioraja@outlook.com>
2023-02-27 14:22:12 -0300

    Cria um objeto de esqueleto para download do INEP

---
+++ src/aquisicao/inep/base_inep.py
@@ -0,0 +1,88 @@
+import abc
+from pathlib import Path
+import typing
+import pandas as pd
+from src.aquisicao.base_etl import BaseETL
+import os
+import urllib
+import requests
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+
+class BaseINEPETL(abc.ABC):
+    """
+    Classe que estrutura como qualquer objeto de ETL
+    deve funcionar para baixar dados do INEP
+    """
+
+    URL: str = (
+        "https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/"
+    )
+
+    def __init__(
+        self, entrada: str, saida: str, base, criar_caminho: bool = True
+    ) -> None:
+        """
+        Instancia objeto de ETL do INEP
+
+        Args:
+            entrada (str): string com o caminho para a pasta de entrada
+            saida (str): string com o caminho para a pasta de saida
+            base (str): Nome da base quer vai para a URL do INEP
+            criar_caminho (bool, optional): flag indicando se devemos criar os caminhos. Defaults to True.
+        """
+        super().__init__(entrada, saida, criar_caminho)
+
+        self._url = f"{self.URL}/{base}"
+
+    def le_pagina_inep(self) -> typing.Dict[str, str]:
+        """Realiza o web-scraping da página de dados do INEP
+        
+        Returns: dicionário com o nome do arquivo e link para a página
+        """
+        html = urllib.request.urlopen(self.url).read()
+        soup = BeautifulSoup(html, features="lxml")
+        return {
+            tag["href"].split("_")[-1]: tag["href"]
+            for tag in soup.find_all("a", {"class": "external-link"})
+        }
+
+    def dicionario_para_baixar(self) -> typing.Dict[str, str]:
+        """Lê os conteúdos da pasta de dados e seleciona apenas os arquivos
+        a serem baixados como complementares
+
+        Returns: dicionário com o nome do arquivo e link para a página
+        """
+        para_baixar = self.le_pagina_inep()
+        baixados = os.listdir(str(self.caminho_entrada))
+        return {arq: link for arq, link in para_baixar.items() if arq not in baixados}
+
+    @abc.abstractmethod
+    def extract(self) -> None:
+        """
+        Extrai os dados do objeto
+        """
+        pass
+
+    @abc.abstractmethod
+    def transform(self) -> None:
+        """
+        Transforma os dados e os adequa para os formatos de saída de interesse
+        """
+        pass
+
+    def load(self) -> None:
+        """
+        Exporta os dados transformados
+        """
+        for arq, df in self.dados_saida.items():
+            df.to_parquet(self.caminho_saida / arq, index=False)
+
+    def pipeline(self) -> None:
+        """
+        Executa o pipeline completo de tratamento de dados
+        """
+        self.extract()
+        self.transform()
+        self.load()
